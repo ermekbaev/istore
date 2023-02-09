72dd513f715dc7f1b4a4a72ad3d288dacd51141b
ermekbaev <ermekbaev176@gmail.com>
2023-02-09 17:49:04 +0600

    new start

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
+
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="flex">
+      <div>
+        <ul>
+          <li>Istore</li>
+          <li>HOME</li>
+          <li>FILTER</li>
+          <li>
+          </li>
+          <li></li>
+        </ul>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
--- src/Components/Products/AddProduct.jsx
+++ src/Components/Products/AddProduct.jsx
@@ -1,7 +1,18 @@
 import React from "react";
 
 const AddProduct = () => {
-  return <div>AddProduct</div>;
+  return (
+    <div>
+      <h2 className="text-3xl">AddProduct</h2>
+      <div>
+        <input className=" m-2" type="text" placeholder="title" />
+        <input className=" m-2" type="text" placeholder="description" />
+        <input className=" m-2" type="text" placeholder="type" />
+        <input className=" m-2" type="text" placeholder="price" />
+        <input className=" m-2" type="text" placeholder="img" />
+      </div>
+    </div>
+  );
 };
 
 export default AddProduct;
