7b2942020d976a2195a19d13db2ed0100730e0d6
ermekbaev <ermekbaev176@gmail.com>
2023-02-09 17:52:14 +0600

    new start

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,16 +1,14 @@
 import React from "react";
 
-
 const Header = () => {
   return (
     <div className="flex">
       <div>
         <ul>
-          <li>Istore</li>
-          <li>HOME</li>
-          <li>FILTER</li>
-          <li>
-          </li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
           <li></li>
         </ul>
       </div>
