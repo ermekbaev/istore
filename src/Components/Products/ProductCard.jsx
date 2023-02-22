877a430f27becbb435ee0b1d49f2a93de2b57de1
ermekbaev <ermekbaev176@gmail.com>
2023-02-22 18:57:38 +0600

    почти финал

--- src/Components/Products/ProductCard.jsx
+++ src/Components/Products/ProductCard.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
 import { useProducts } from "../../Contexts/ProductContextProvider";
+import { AiOutlineDelete, AiOutlineShopping } from "react-icons/ai";
+import { useCart } from "../../Contexts/CartContextProduct";
+import { useAuth } from "../../Contexts/AuthContextProvider";
 
 const ProductCard = ({ item }) => {
   const { deleteProduct } = useProducts();
+  // const { addProductToCart, checkProductInCart } = useCart();
+
   const navigate = useNavigate();
-  console.log(item, "ffffffffffffffffffffffffff");
+
   return (
     <div className="border border-dark m-3">
       <img src={item.image} width="200" alt="" />
@@ -13,18 +18,23 @@ const ProductCard = ({ item }) => {
       <p>{item.price}</p>
       <p>{item.category}</p>
       <p>{item.description}</p>
-      <p>{item.stock}</p>
-      <p>{item.owner_email}</p>
+      {/* <p>{item.stock}</p> */}
+      {/* <p>{item.owner_email}</p> */}
 
-      <button onClick={() => navigate(`/edit/${item.id}`)}>Edit</button>
-      <button onClick={() => deleteProduct(item.id)}>Delete</button>
-
-      {/* {item.is_author ? (
+      {item.is_author ? (
         <>
-          <button onClick={() => navigate(`/edit/${item.id}`)}>Edit</button>
-          <button onClick={() => deleteProduct(item.id)}>Delete</button>
+          <button
+            className="m-2 bg-slate-600 w-20 text-white border-gray-300 rounded-md
+                text-sm "
+            onClick={() => navigate(`/edit/${item.id}`)}
+          >
+            Edit
+          </button>
+          <button className="m-2" onClick={() => deleteProduct(item.id)}>
+            <AiOutlineDelete />
+          </button>
         </>
-      ) : null} */}
+      ) : null}
     </div>
   );
 };
